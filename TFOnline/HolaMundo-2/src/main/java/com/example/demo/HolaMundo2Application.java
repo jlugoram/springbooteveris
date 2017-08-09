85eab806928278dc7cca532973f7a309e584352e
Jose Eduardo Lugo Ramos <jose.eduardo.lugo.ramos.sa@everis.com>
2017-08-09 17:34:27 -0500

    Se crea el microservicio

---
+++ TFOnline/HolaMundo-2/src/main/java/com/example/demo/HolaMundo2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundo2Application.class, args);
+	}
+}
